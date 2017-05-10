0867285b3950ec7191ce4a7a9b9f6f3e225cb3ea
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-05-10 14:46:16 +0900

    histogram: Add multi-threaded one

--- src/histogram.c
+++ src/histogram.c
@@ -10,24 +10,45 @@
 #include "rpiraw.h"
 #include <stdint.h>
 #include <string.h>
+#include <omp.h>
 
 int rpiraw_calc_histogram_rgb888(uint32_t hist_r[256], uint32_t hist_g[256],
                                  uint32_t hist_b[256],
                                  uint8_t *img, const unsigned ld,
                                  const unsigned width, const unsigned height)
 {
-    unsigned i, j;
+#pragma omp parallel firstprivate(img)
+    {
+        unsigned i, j;
+        const int nthreads = omp_get_num_threads();
+        const int num = omp_get_thread_num();
+        const unsigned i_start = height *  num      / nthreads;
+        const unsigned i_end   = height * (num + 1) / nthreads;
+        uint32_t hist_r_priv[256], hist_g_priv[256], hist_b_priv[256];
 
-    memset(hist_r, 0, 256 * sizeof(*hist_r));
-    memset(hist_g, 0, 256 * sizeof(*hist_g));
-    memset(hist_b, 0, 256 * sizeof(*hist_b));
-    for (i = 0; i < height; i ++) {
-        for (j = 0; j < width; j ++) {
-            hist_r[*img++]++;
-            hist_g[*img++]++;
-            hist_b[*img++]++;
+        memset(hist_r_priv, 0, 256 * sizeof(*hist_r_priv));
+        memset(hist_g_priv, 0, 256 * sizeof(*hist_g_priv));
+        memset(hist_b_priv, 0, 256 * sizeof(*hist_b_priv));
+        img += i_start * ld * 3;
+
+        for (i = i_start; i < i_end; i ++) {
+            for (j = 0; j < width; j ++) {
+                hist_r_priv[*img++]++;
+                hist_g_priv[*img++]++;
+                hist_b_priv[*img++]++;
+            }
+            img += (ld - width) * 3;
         }
-        img += (ld - width) * 3;
+
+#pragma omp critical
+        for (i = 0; i < 256; i ++)
+            hist_r[i] += hist_r_priv[i];
+#pragma omp critical
+        for (i = 0; i < 256; i ++)
+            hist_g[i] += hist_g_priv[i];
+#pragma omp critical
+        for (i = 0; i < 256; i ++)
+            hist_b[i] += hist_b_priv[i];
     }
 
     return 0;
