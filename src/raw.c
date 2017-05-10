7612efd14569c3499b8ec6a1b16bbbbef4a4810d
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-05-10 16:17:36 +0900

    raw: Add a comment for rpi-specific raw_width calculation

--- src/raw.c
+++ src/raw.c
@@ -13,5 +13,10 @@
 
 unsigned rpiraw_width_raw8_to_raw10_rpi(const unsigned width)
 {
+    /*
+     * MIPI specifies the size be height * width * 10 / 8.
+     * However, the firmware of Raspberry Pi uses the equation below.
+     * See https://github.com/6by9/RPiTest/commit/d66f1454e1986d137d040a36e1d2f6b5a3217259#commitcomment-21443216
+    */
     return ALIGN_UP((width * 5 + 3) >> 2, 32);
 }
