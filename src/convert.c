68fa296d74ed5dee3f27cda5754737b534168fce
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-05-10 22:42:13 +0900

    convert_raw10_to_raw8: Add multi-threaded one

--- src/convert.c
+++ src/convert.c
@@ -9,27 +9,38 @@
 
 #include "rpiraw.h"
 #include <stdint.h>
+#include <omp.h>
 
 int rpiraw_convert_raw10_to_raw8(uint8_t *dst, uint8_t *src,
                                  const unsigned width, const unsigned height,
                                  const unsigned raw_width)
 {
-    unsigned i, j;
-
     /* As specified in MIPI Alliance Specification for CSI-2. */
 
     if (width % 4 != 0)
         return 1;
 
-    for (i = 0; i < height; i ++) {
-        for (j = 0; j < width / 4; j ++) {
-            *dst++ = *src++;
-            *dst++ = *src++;
-            *dst++ = *src++;
-            *dst++ = *src++;
-            src++;
+#pragma omp parallel firstprivate(dst, src)
+    {
+        unsigned i, j;
+        const int nthreads = omp_get_num_threads();
+        const int num = omp_get_thread_num();
+        const unsigned i_start = height *  num      / nthreads,
+                       i_end   = height * (num + 1) / nthreads;
+
+        dst += i_start * width;
+        src += i_start * raw_width;
+
+        for (i = i_start; i < i_end; i ++) {
+            for (j = 0; j < width / 4; j ++) {
+                *dst++ = *src++;
+                *dst++ = *src++;
+                *dst++ = *src++;
+                *dst++ = *src++;
+                src++;
+            }
+            src += raw_width - width * 10 / 8;
         }
-        src += raw_width - width * 10 / 8;
     }
 
     return 0;
